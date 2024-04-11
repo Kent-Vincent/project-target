110b5cfd49d596713d67d1367dd40d200e7e7753
Kent-Vincent <sarsalejo02@gmail.com>
2024-04-11 10:18:24 +0800

    added pages

---
+++ src/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalDeleteComponent } from './modals/modal-delete/modal-delete.component';
+
+
+
+@NgModule({
+  declarations: [
+    ModalDeleteComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
