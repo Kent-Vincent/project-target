15ca5fed0cb1052f0bdf68104f650ac5c2420d6f
hrjugar <hernanjugar123@gmail.com>
2024-04-11 15:59:52 +0800

    Built skeleton of top navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TopNavLayoutComponent } from './layouts/top-nav-layout/top-nav-layout.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
+  // {
+  //   path: '',
+  //   redirectTo: 'login',
+  //   pathMatch: 'full'
+  // },
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
@@ -15,6 +16,10 @@ const routes: Routes = [
     path: 'signup',
     loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupModule)
   },
+  {
+    path: '',
+    loadChildren: () => import('./layouts/top-nav-layout/top-nav-layout.module').then(m => m.TopNavLayoutModule)
+  }
 ];
 
 @NgModule({
