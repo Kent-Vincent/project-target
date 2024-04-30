881492089ebe2f59a267c2d518b5bd490ed906bd
Kent-Vincent <sarsalejo02@gmail.com>
2024-04-30 10:46:01 +0800

    turned on auth guard

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-// import { AuthService } from 'src/app/commons/services/auth.service';
+import { AuthService } from 'src/app/commons/services/auth.service';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
@@ -11,29 +11,28 @@ export class LoginComponent {
   email: string = '';
   password: string = '';
 
-  constructor (private router: Router) {} 
-  // constructor(private authService: AuthService, private router: Router) {} 
+  constructor(private authService: AuthService, private router: Router) {} 
 
-  // ngOnInit(): void{
-  //   const token = localStorage.getItem('token');
-  //   if (token){
-  //     this.router.navigate(['/workspace']);
-  //   }
-  // }
+  ngOnInit(): void{
+    const token = localStorage.getItem('token');
+    if (token){
+      this.router.navigate(['/workspace']);
+    }
+  }
 
   
-  // login(): void {
-  //   this.authService.login(this.email, this.password)
-  //     .subscribe(
-  //       (response) => {
-  //         localStorage.setItem('token', response.token);
-  //         this.router.navigate(['/workspace'])
-  //       },
-  //       (error) => {
-  //         console.log(error.error);
-  //       }
-  //     );
-  // }
+  login(): void {
+    this.authService.login(this.email, this.password)
+      .subscribe(
+        (response) => {
+          localStorage.setItem('token', response.token);
+          this.router.navigate(['/workspace'])
+        },
+        (error) => {
+          console.log(error.error);
+        }
+      );
+  }
 
   
 
