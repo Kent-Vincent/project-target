24b8537950a70a2aec51239138491b6303ceb97a
Kent-Vincent <sarsalejo02@gmail.com>
2024-05-07 14:59:08 +0800

    created creating workspace function

--- src/app/commons/services/workspace.service.ts
+++ src/app/commons/services/workspace.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthTokenService } from './auth-token.service';
-import { API_WORKSPACE_ID } from '../constants/api.constant';
+import { API_WORKSPACE_CREATE, API_WORKSPACE_ID } from '../constants/api.constant';
+import { Workspace } from '../models/workspace.model';
+import { CurrentUserService } from './current-user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +19,13 @@ export class WorkspaceService {
       'Authorization': `Token ${this.authToken}`
     });
     return this.http.get<any>(API_WORKSPACE_ID, { headers: headers});
+  }
+
+  createWorkspace(data:Workspace|FormData) {
+    const headers = new HttpHeaders({
+      'Authorization': `Token ${this.authToken}`
+    })
+    return this.http.post<any>(API_WORKSPACE_CREATE, data, {headers: headers});
   }
 
   // getAllUsers(workspaceId: number): Observable<any> {
