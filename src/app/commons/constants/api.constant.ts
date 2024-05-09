54d38058c220a04ff6ef475c5ed0283c72e3e050
Kent-Vincent <sarsalejo02@gmail.com>
2024-05-09 20:23:24 +0800

    API renamed for fetching stages and service function altered for stage

--- src/app/commons/constants/api.constant.ts
+++ src/app/commons/constants/api.constant.ts
@@ -21,7 +21,7 @@ export const API_WORKSPACE_SPECIFIC_ID = urlEncode([API_URL, 'workspace']);
 
 
 // Fetching stage base on user ID
-export const API_STAGE_ID = urlEncode([API_WORKSPACE, 'stage']);
+export const API_STAGE_SPECIFIC_ID = urlEncode([API_WORKSPACE_SPECIFIC_ID, 'stages']);
 
 
 // Fetching current user logged in
