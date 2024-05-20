7d6bfc8beefeb3705d037456f64e17df41ce830d
Kent-Vincent <sarsalejo02@gmail.com>
2024-05-20 10:23:03 +0800

    improved UI and after adding workspace it will display right after

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { WorkspaceService } from 'src/app/commons/services/workspace.service';
+import { CreateWorkspaceModalComponent } from 'src/shared/create-workspace-modal/create-workspace-modal.component';
 
 @Component({
   selector: 'app-boards',
@@ -10,17 +12,35 @@ import { WorkspaceService } from 'src/app/commons/services/workspace.service';
 export class BoardsComponent {
   workspaces: { name: string, ID: number }[] = [];
 
-  constructor(private workspaceService: WorkspaceService, private router: Router) {}
+  constructor(
+    private workspaceService: WorkspaceService,
+    private router: Router,
+    public dialog: MatDialog,
+    ) {}
 
   ngOnInit(): void {
+    this.loadWorkspaces();
+  }
+
+  loadWorkspaces(): void {
     this.workspaceService.getWorkspaces().subscribe(
       (workspaces: { name: string, ID: number }[]) => {
         this.workspaces = workspaces;
       },
       (error) => {
-        console.error('Error fetching workspaces:', error);
+        console.error('Error fetching workspaces:', error.error);
       }
     );
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(CreateWorkspaceModalComponent, {});
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === 'created') {
+        this.loadWorkspaces();
+      }
+    });
   }
 
   workspaceID(workspaceIndex: number): void {
